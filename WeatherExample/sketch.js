9ee42d2aaa7429aae1f930997d4bbe6a45b03eec
William <wfsaunders@gmail.com>
2015-07-30 13:25:38 +0200

    1

---
+++ WeatherExample/sketch.js
@@ -0,0 +1,57 @@
+//A wind direction vector
+var wind;
+// Circle position
+var position;
+// background transprency
+var humidity;
+// red value
+var temp;
+
+var sunMan;
+var cloudMan;
+var rainMan;
+var img1;
+
+
+
+function setup() {
+  createCanvas(720, 400);
+
+sunMan = loadImage("assets/sunMan.gif");
+cloudMan = loadImage("assets/cloudMan.gif");
+rainMan = loadImage("assets/rainMan.gif");
+img1 = loadGif("assets/img1.jpg");
+
+
+
+
+}
+
+function draw() {
+
+   var imageDiv = createDiv(image(rainMan, 0, 0));
+
+
+
+}
+
+function gif() {
+
+
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
